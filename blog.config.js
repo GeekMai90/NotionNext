5d9dd1f8dc524e9c8d6cf54d9bc058d05912fb3e
GeekMai90 <166203003+GeekMai90@users.noreply.github.com>
2026-01-20 13:31:25 +0800

    Update author bio and website link in config

--- blog.config.js
+++ blog.config.js
@@ -15,8 +15,8 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '麦先生说', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Anteey AI独立开发者 | 知识管理爱好者', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://talk.anteey.com', // 网站地址
+  BIO: process.env.NEXT_PUBLIC_BIO || '知识管理爱好者 | 效率软件爱好者 | 喜欢折腾一切好奇的事物', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://geekmai.cc', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, Anteey, Antinet, Zettelkasten, 卡片盒笔记法', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '备案号 吉ICP备2025024553号', // 备案号 闽ICP备XXXXXX
